78a6c3345fd4d4dd6c5e063c95f5d1b156208c21
Win Myo Htet <wmhtet@aunndroid.com>
2014-04-22 11:10:42 -0700

    linux version

--- DisplayImage/header/DisplayImage.h
+++ DisplayImage/header/DisplayImage.h
@@ -1,17 +1,19 @@
 /*
  * DisplayImage.h
  *
- *  Created on: Apr 19, 2014
- *      Author: aunnaunn
+ *  Created on: Apr 17, 2014
+ *      Author: whtet
  */
 
 #ifndef DISPLAYIMAGE_H_
 #define DISPLAYIMAGE_H_
 
+
+int displayPic(int argc, char** argv);
+
 int displayMovie(int argc, char** argv);
-
-int displayImage(int argc, char** argv);
-
+int displayMovieBlurr(int argc, char** argv);
+int displayMovieTracker(int argc, char** argv);
 
 
 #endif /* DISPLAYIMAGE_H_ */
--- DisplayImage/src/DisplayImage.cpp
+++ DisplayImage/src/DisplayImage.cpp
@@ -1,39 +1,71 @@
-#include <opencv2/opencv.hpp>
-#include <opencv/highgui.h>
-#include <stdio.h>
+#include <cv.hpp>
+#include <highgui.h>
 #include <DisplayImage.h>
-
+//   /home/whtet/temp/archive/faceDetect_sample/lena.jpg
+//   /home/whtet/temp/video/PPV.mp4
 using namespace cv;
 
+static const int DISPLAY_PICTURE = 0;
+static const int DISPLAY_MOVIE = 1;
+static const int DISPLAY_MOVIE_BLURR = 2;
+static const int DISPLAY_MOVIE_TRACKER = 3;
+
 int main(int argc, char** argv) {
-	bool movie = true;
-	if (movie)
-		displayMovie(argc, argv);
-	else
-		displayImage(argc, argv);
+	int methodId = DISPLAY_MOVIE_TRACKER;
+	switch (methodId) {
+	case DISPLAY_PICTURE:
+		return displayPic(argc, argv);
+	case DISPLAY_MOVIE:
+		return displayMovie(argc, argv);
+	case DISPLAY_MOVIE_BLURR:
+		return displayMovieBlurr(argc, argv);
+	case DISPLAY_MOVIE_TRACKER:
+		return displayMovieTracker(argc, argv);
+	default:
+		printf("No such method %d %s \n", methodId, argv[1]);
+		break;
+	}
 }
 
-int displayImage(int argc, char** argv) {
+int gSliderCurrPosition = 0;
+VideoCapture *gVcapTracker;
 
-	printf("Hello World \n");
+void onTrackbarChanged(int pos, void* _void) {
+	//printf("on_trackbar %d %d \n", pos, gSliderCurrPosition);
+	if (gSliderCurrPosition != pos) {
+		gVcapTracker->set(CV_CAP_PROP_POS_FRAMES, pos);
+		//bool flag = //printf("on_trackbar %d %s \n", pos, flag ? "true" : "false");
+	}
+}
+
+int displayMovieTracker(int argc, char** argv) {
 	Mat image;
-	printf("Hello World 1\n");
-	image = imread(argv[1], 1);
-	printf("Hello World 2 \n");
+	int gSlider_position = 0;
+	gVcapTracker = new VideoCapture(argv[1]);
+	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
 
-	if (argc != 2 || !image.data) {
-		printf("No image data \n");
-		return -1;
+	int frames = (int) gVcapTracker->get(CV_CAP_PROP_FRAME_COUNT);
+
+	if (frames != 0) {
+		createTrackbar("Position", "Display Image", &gSlider_position, frames,
+				onTrackbarChanged);
 	}
 
-	printf("Hello World 3\n");
-	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
-	printf("Hello World 4\n");
-	imshow("Display Image", image);
-
-	printf("Hello World 5\n");
-	waitKey(0);
-
+	while (1) {
+		gVcapTracker->read(image);
+		setTrackbarPos("Position", "Display Image", gSlider_position);
+		gSlider_position++;
+		gSliderCurrPosition = gSlider_position;
+		if (!image.data) {
+			printf("Video data %s %d \n", argv[1], frames);
+			break;
+		}
+		imshow("Display Image", image);
+		if (waitKey(33) == 27)
+			break;
+	}
+	gVcapTracker->release();
+	destroyWindow("Display Image");
 	return 0;
 }
 
@@ -58,5 +90,59 @@ int displayMovie(int argc, char** argv) {
 	}
 	vcap->release();
 	destroyWindow("Display Image");
+	return 0;
+}
+
+int displayMovieBlurr(int argc, char** argv) {
+	//VideoCapture *vcap = new VideoCapture(argv[1]);
+	String path = "/home/whtet/temp/video/PPV.mp4";
+	VideoCapture cap(path);
+	Mat image;
+	Mat edges;
+	cap.open(path);
+	if (!cap.isOpened()) {
+		printf("Cannot open %s \n", path.c_str());
+		return -1;
+	}
+	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+	int count = 0;
+
+	while (1) {
+		//vcap->grab();
+		//vcap->read(image);
+		Mat frame;
+		cap >> frame;
+		if (!frame.data) {
+			printf("No video data %s %d \n", path.c_str(), count);
+			break;
+		}
+		cvtColor(frame, edges, CV_BGR2GRAY);
+		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		Canny(edges, edges, 0, 30, 3);
+		imshow("Display Image", edges);
+		if (waitKey(33) == 27)
+			break;
+		count++;
+	}
+	//vcap->release();
+	cap.release();
+	destroyWindow("Display Image");
+	return 0;
+}
+
+int displayPic(int argc, char** argv) {
+	Mat image;
+	image = imread(argv[1], 1);
+
+	if (argc != 2 || !image.data) {
+		printf("No image data \n");
+		return -1;
+	}
+
+	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+
+	waitKey(0);
+
 	return 0;
 }
